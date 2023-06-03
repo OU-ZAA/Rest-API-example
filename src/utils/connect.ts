5058943336e33770cd6480c32dc7422faba5f611
Mohamed ouzaa <ouzaa.contact@gmail.com>
2023-06-03 14:26:39 +0100

    setup api

---
+++ src/utils/connect.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import config from "config";
+
+function connect() {
+  const dbUri = config.get<string>("dbUri");
+
+  return mongoose
+    .connect(dbUri)
+    .then(() => {
+      console.log("db connected...");
+    })
+    .catch((error) => {
+      console.error("Could not connect...");
+      process.exit(1);
+    });
+}
+
+export default connect;
