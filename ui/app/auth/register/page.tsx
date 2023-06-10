9b2a8572145b7a73900df9d5d67508d982e78d51
Mohamed ouzaa <ouzaa.contact@gmail.com>
2023-06-10 17:56:33 +0100

    added form validation

--- ui/app/auth/register/page.tsx
+++ ui/app/auth/register/page.tsx
@@ -1,90 +1,144 @@
 "use client";
 import { useForm, SubmitHandler } from "react-hook-form";
-import z from "zod"
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-fonst FormInput = {
-  z.object()
-}
+const formSchema = z
+  .object({
+    username: z.string().min(1, "Username is required"),
+    email: z.string().min(1, "Username is required").email("Not a valid email"),
+    password: z
+      .string()
+      .min(1, "Username is required")
+      .min(6, "Password to short -should be 6 chars minimum"),
+    confirmPassword: z.string().min(1, "Username is required"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
+
+type FormSchemaType = z.infer<typeof formSchema>;
 
 export default function Register() {
-  const { register, handleSubmit } = useForm();
-  const onSubmit: SubmitHandler<FormInput> = (data) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<FormSchemaType>({
+    resolver: zodResolver(formSchema),
+  });
+
+  const onSubmit: SubmitHandler<FormSchemaType> = (data) => {
     console.log(data);
   };
 
   return (
     <>
-      <div className="h-screen grid place-content-center">
-        <div className="w-full max-w-xs">
-          <form
-            className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
-            onSubmit={handleSubmit(onSubmit)}
-          >
-            <div className="mb-4">
-              <label
-                className="block text-gray-700 text-sm font-bold mb-2"
-                htmlFor="username"
+      <section className="bg-gray-50">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+          <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+                Create and account
+              </h1>
+              <form
+                className="space-y-4 md:space-y-6"
+                onSubmit={handleSubmit(onSubmit)}
               >
-                Username
-              </label>
-              <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="username"
-                type="text"
-                placeholder="Username"
-                {...register("username")}
-              />
+                <div>
+                  <label
+                    htmlFor="username"
+                    className="block mb-2 text-sm font-medium text-gray-900"
+                  >
+                    Your username
+                  </label>
+                  <input
+                    type="text"
+                    id="username"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
+                    placeholder="Your name"
+                    {...register("username")}
+                  />
+                  {errors.username && (
+                    <span className="text-red-800 block mt-2">
+                      {errors.username?.message}
+                    </span>
+                  )}
+                </div>
+                <div>
+                  <label
+                    htmlFor="email"
+                    className="block mb-2 text-sm font-medium text-gray-900"
+                  >
+                    Your email
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
+                    placeholder="name@company.com"
+                    {...register("email")}
+                  />
+                  {errors.email && (
+                    <span className="text-red-800 block mt-2">
+                      {errors.email?.message}
+                    </span>
+                  )}
+                </div>
+                <div>
+                  <label
+                    htmlFor="password"
+                    className="block mb-2 text-sm font-medium text-gray-900"
+                  >
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    id="password"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
+                    placeholder="••••••••"
+                    {...register("password")}
+                  />
+                  {errors.password && (
+                    <span className="text-red-800 block mt-2">
+                      {errors.password?.message}
+                    </span>
+                  )}
+                </div>
+                <div>
+                  <label
+                    htmlFor="confirm-password"
+                    className="block mb-2 text-sm font-medium text-gray-900"
+                  >
+                    Confirm password
+                  </label>
+                  <input
+                    type="password"
+                    id="confirmPassword"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
+                    placeholder="••••••••"
+                    {...register("confirmPassword")}
+                  />
+                  {errors.confirmPassword && (
+                    <span className="text-red-800 block mt-2">
+                      {errors.confirmPassword?.message}
+                    </span>
+                  )}
+                </div>
+
+                <button
+                  type="submit"
+                  className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center disabled:opacity-25"
+                  disabled={isSubmitting}
+                >
+                  Create an account
+                </button>
+              </form>
             </div>
-            <div className="mb-6">
-              <label
-                className="block text-gray-700 text-sm font-bold mb-2"
-                htmlFor="password"
-              >
-                Password
-              </label>
-              <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                id="password"
-                type="password"
-                placeholder="******************"
-                {...register("password")}
-              />
-            </div>
-            <div className="mb-6">
-              <label
-                className="block text-gray-700 text-sm font-bold mb-2"
-                htmlFor="confirmPassword"
-              >
-                Password
-              </label>
-              <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                id="confirmPassword"
-                type="password"
-                placeholder="******************"
-                {...register("confirmPassword")}
-              />
-            </div>
-            <div className="flex items-center justify-between">
-              <button
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                type="submit"
-              >
-                Sign In
-              </button>
-              <a
-                className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-                href="#"
-              >
-                Forgot Password?
-              </a>
-            </div>
-          </form>
-          <p className="text-center text-gray-500 text-xs">
-            &copy;2020 Acme Corp. All rights reserved.
-          </p>
+          </div>
         </div>
-      </div>
+      </section>
     </>
   );
 }
