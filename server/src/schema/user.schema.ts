af5a8b137c446680bd397f9792b5af2bb3d29480
Mohamed ouzaa <ouzaa.contact@gmail.com>
2023-06-10 17:19:53 +0100

    fix typos

--- server/src/schema/user.schema.ts
+++ server/src/schema/user.schema.ts
@@ -6,13 +6,13 @@ export const createUserSchema = object({
       required_error: "Email is required",
     }).email("Not a valide email"),
     name: string({
-      required_error: "Email is required",
+      required_error: "Name is required",
     }),
     password: string({
-      required_error: "Email is required",
+      required_error: "Password is required",
     }).min(6, "Password to short -should be 6 chars minimum"),
     passwordConfirmation: string({
-      required_error: "Email is required",
+      required_error: "Confirm password is required",
     }),
   }).refine((data) => data.password === data.passwordConfirmation, {
     message: "Passwords do not match",
