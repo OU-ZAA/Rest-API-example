953e32e4284ed5139318c04b0c59fad66cf701b0
Mohamed ouzaa <ouzaa.contact@gmail.com>
2023-06-11 16:35:31 +0100

    added a get the current user route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,8 @@
-import { Express, Request, Response } from "express";
-import { createUserHandler } from "./controller/user.controller";
+import { Express } from "express";
+import {
+  createUserHandler,
+  getCurrentUser,
+} from "./controller/user.controller";
 import { createUserSchema } from "./schema/user.schema";
 import validateResource from "./middleware/validateResource";
 import { createSessionSchema } from "./schema/session.schema";
@@ -23,9 +26,9 @@ import {
 } from "./controller/product.controller";
 
 function routes(app: Express) {
-  app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200));
-
   app.post("/api/users", validateResource(createUserSchema), createUserHandler);
+
+  app.get("/api/me", requireUser, getCurrentUser);
 
   app.post(
     "/api/sessions",
