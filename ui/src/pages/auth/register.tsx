ecef6e4c3b37e24ae3e30e4d8ac40784fef9fa84
Mohamed ouzaa <ouzaa.contact@gmail.com>
2023-06-11 16:12:31 +0100

    switched to pages route

--- ui/src/pages/auth/register.tsx
+++ ui/src/pages/auth/register.tsx
@@ -2,35 +2,50 @@
 import { useForm, SubmitHandler } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
+import { useState } from "react";
 
-const formSchema = z
+const createUserSchema = z
   .object({
-    username: z.string().min(1, "Username is required"),
-    email: z.string().min(1, "Username is required").email("Not a valid email"),
+    username: z.string().nonempty({ message: "Username is required" }),
+    email: z
+      .string()
+      .nonempty({ message: "Email is required" })
+      .email("Not a valid email"),
     password: z
       .string()
-      .min(1, "Username is required")
+      .nonempty({ message: "Password is required" })
       .min(6, "Password to short -should be 6 chars minimum"),
-    confirmPassword: z.string().min(1, "Username is required"),
+    confirmPassword: z
+      .string()
+      .nonempty({ message: "Confirm password is required" }),
   })
   .refine((data) => data.password === data.confirmPassword, {
     message: "Passwords do not match",
     path: ["confirmPassword"],
   });
 
-type FormSchemaType = z.infer<typeof formSchema>;
+type CreateUserInput = z.infer<typeof createUserSchema>;
 
 export default function Register() {
+  const [registerError, setRegisterError] = useState(null);
   const {
     register,
     handleSubmit,
     formState: { errors, isSubmitting },
-  } = useForm<FormSchemaType>({
-    resolver: zodResolver(formSchema),
+  } = useForm<CreateUserInput>({
+    resolver: zodResolver(createUserSchema),
   });
 
-  const onSubmit: SubmitHandler<FormSchemaType> = (data) => {
-    console.log(data);
+  const onSubmit: SubmitHandler<CreateUserInput> = async (data) => {
+    try {
+      await axios.post(
+        `${process.env.NEXT_PUBLIC_SERVER_ENDPOINT}/api/users`,
+        data
+      );
+    } catch (e: any) {
+      setRegisterError(e.message);
+    }
   };
 
   return (
@@ -42,6 +57,7 @@ export default function Register() {
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
                 Create and account
               </h1>
+              <p>{registerError}</p>
               <form
                 className="space-y-4 md:space-y-6"
                 onSubmit={handleSubmit(onSubmit)}
@@ -60,11 +76,9 @@ export default function Register() {
                     placeholder="Your name"
                     {...register("username")}
                   />
-                  {errors.username && (
-                    <span className="text-red-800 block mt-2">
-                      {errors.username?.message}
-                    </span>
-                  )}
+                  <span className="text-red-800 text-sm block mt-2">
+                    {errors.username?.message}
+                  </span>
                 </div>
                 <div>
                   <label
@@ -77,14 +91,12 @@ export default function Register() {
                     type="email"
                     id="email"
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
-                    placeholder="name@company.com"
+                    placeholder="Ouzaa@example.com"
                     {...register("email")}
                   />
-                  {errors.email && (
-                    <span className="text-red-800 block mt-2">
-                      {errors.email?.message}
-                    </span>
-                  )}
+                  <span className="text-red-800 text-sm block mt-2">
+                    {errors.email?.message}
+                  </span>
                 </div>
                 <div>
                   <label
@@ -100,11 +112,9 @@ export default function Register() {
                     placeholder="••••••••"
                     {...register("password")}
                   />
-                  {errors.password && (
-                    <span className="text-red-800 block mt-2">
-                      {errors.password?.message}
-                    </span>
-                  )}
+                  <span className="text-red-800 text-sm block mt-2">
+                    {errors.password?.message}
+                  </span>
                 </div>
                 <div>
                   <label
@@ -120,16 +130,13 @@ export default function Register() {
                     placeholder="••••••••"
                     {...register("confirmPassword")}
                   />
-                  {errors.confirmPassword && (
-                    <span className="text-red-800 block mt-2">
-                      {errors.confirmPassword?.message}
-                    </span>
-                  )}
+                  <span className="text-red-800 text-sm block mt-2">
+                    {errors.confirmPassword?.message}
+                  </span>
                 </div>
-
                 <button
                   type="submit"
-                  className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center disabled:opacity-25"
+                  className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center disabled:opacity-25"
                   disabled={isSubmitting}
                 >
                   Create an account
