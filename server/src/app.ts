3383b1a535f9acf9746cb356317c274007b61e3b
Mohamed ouzaa <ouzaa.contact@gmail.com>
2023-06-12 01:48:50 +0100

    added cookie-parser

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,11 +5,13 @@ import connect from "./utils/connect";
 import logger from "./utils/logger";
 import routes from "./routes";
 import deserializeUser from "./middleware/deserializeUser";
+import cookieParser from "cookie-parser";
 
 const port = config.get<number>("port");
 
 const app = express();
 
+app.use(cookieParser());
 app.use(
   cors({
     origin: config.get<string>("origin"),
